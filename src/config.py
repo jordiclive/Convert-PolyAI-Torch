07034b6de75750172a7dd5a023ec914bab53de74
Jordi Clive <jordiclive@protonmail.com>
2020-08-29 17:07:14 +0100

    sanity check (+1 squashed commit) Squashed commits: [3538463] first commit

---
+++ src/config.py
@@ -0,0 +1,50 @@
+import os
+from typing import NamedTuple
+
+dirname, _ = os.path.split(os.path.dirname(__file__))
+
+class ConveRTModelConfig(NamedTuple):
+
+    num_embed_hidden: int = 512
+    feed_forward1_hidden: int = 2048
+    feed_forward2_hidden: int = 1024
+    num_attention_project: int = 64
+    vocab_size: int = 25000
+    num_encoder_layers: int = 6
+    dropout_rate: float = 0.0
+    n: int = 121
+    relative_attns: list = [3, 5, 48, 48, 48, 48]
+    num_attention_heads: int = 2
+    token_sequence_truncation: int = 60
+
+
+class ConveRTTrainConfig(NamedTuple):
+
+    sp_model_path: str = os.path.join(dirname, "data/en.wiki.bpe.vs25000.model")
+    dataset_path: str = os.path.join(dirname, "data/sample-dataset.json")
+    test_dataset_path: str = "data/sample-dataset.json"
+
+    model_save_dir: str = "lightning_logs/checkpoints/"
+    log_dir: str = "lightning_logs"
+    device: str = "cuda"
+    use_data_paraller: bool = True
+
+    is_reddit: bool = True
+
+    train_batch_size: int = 64
+    test_batch_size: int = 256
+
+    split_size: int = 8
+    learning_rate: float = 1e-3  # final learning rate ie 'lr annealed to'
+    lr_warmup_start: float = 0.1  # start of lr before initial linear warmup section
+    lr_warmup_end: float = 1.0  # end of linear warmup section , annealing begin
+    warmup_batch: float = 10000  # how many batches linear warm up for
+    final_batch: float = 1e8  # final batch of training when want learning rate
+    learning_rate_end: float = 0.0001
+    epochs: int = 10
+    grad_norm_clip: float = 1.0
+    smoothing: float = 0.2
+    l2_weight_decay: float = 1e-5  # note: different from L2 reg, as working with Adam. L2 regularization
+    # (or any lagrange m on loss) not wise
+
+
---
+++ src/dataset.py
@@ -0,0 +1,165 @@
+import json
+from dataclasses import dataclass
+from typing import List, NamedTuple
+
+import pytorch_lightning as pl
+import torch
+from sentencepiece import SentencePieceProcessor
+from torch.nn.functional import pad
+from torch.utils.data import DataLoader
+from src.config import ConveRTTrainConfig
+
+
+config = ConveRTTrainConfig()
+
+
+# Todo implement BPE from scratch with unk tokens hashed (although may achieve worse results on downstream tasks) as
+#  perhaps not as general as bpemb's 25000.model
+
+
+@dataclass
+class EncoderInputFeature:
+    input_ids: torch.Tensor
+    attention_mask: torch.Tensor
+    position_ids: torch.Tensor
+    input_lengths: torch.Tensor
+
+    def pad_sequence(self, seq_len: int):
+        self.input_ids = pad(
+            self.input_ids, [0, seq_len - self.input_ids.size(0)], "constant", 0
+        )
+        self.attention_mask = pad(
+            self.attention_mask,
+            [0, seq_len - self.attention_mask.size(0)],
+            "constant",
+            0,
+        )
+        self.position_ids = pad(
+            self.position_ids, [0, seq_len - self.position_ids.size(0)], "constant", 0
+        )
+
+
+@dataclass
+class EmbeddingPair:
+    context: EncoderInputFeature
+    reply: EncoderInputFeature
+
+
+class DataModule(pl.LightningDataModule):
+    # using pytorch lightning, as will save a lot of time downstream, when using multi-gpu, distributed method
+    # and for managing 16 precision.
+    def __init__(self):
+        super().__init__()
+        self.input_attributes = [
+            "input_ids",
+            "attention_mask",
+            "position_ids",
+            "input_lengths",
+        ]
+
+    def batching_input_features(
+            self, encoder_inputs: List[EncoderInputFeature]
+    ) -> EncoderInputFeature:
+        max_seq_len = max(
+            [
+                int(encoder_input.input_lengths.item())
+                for encoder_input in encoder_inputs
+            ]
+        )
+        for encoder_input in encoder_inputs:
+            encoder_input.pad_sequence(max_seq_len)
+
+        batch_features = {
+            feature_name: torch.stack(
+                [
+                    getattr(encoder_input, feature_name)
+                    for encoder_input in encoder_inputs
+                ],
+                dim = 0,
+            )
+            for feature_name in self.input_attributes
+        }
+        return EncoderInputFeature(**batch_features)
+
+    def convert_collate_fn(self, features: List[EmbeddingPair]) -> EmbeddingPair:
+        return EmbeddingPair(
+            context = self.batching_input_features(
+                [feature.context for feature in features]
+            ),
+            reply = self.batching_input_features([feature.reply for feature in features]),
+        )
+
+    def train_dataloader(self, train_dataset):
+        return DataLoader(
+            train_dataset,
+            config.train_batch_size,
+            collate_fn = self.convert_collate_fn,
+            drop_last = True,     # drop last incomplete batch
+
+        )
+
+
+    def val_dataloader(self):
+        # Todo
+        pass
+        # return DataLoader()
+
+    def test_dataloader(self):
+        # Todo
+        pass
+        # return DataLoader()
+
+
+class DatasetInstance(NamedTuple):
+    context: List[str]
+    response: str
+
+
+def load_instances_from_reddit_json(dataset_path: str) -> List[DatasetInstance]:
+    instances: List[DatasetInstance] = []
+    with open(dataset_path) as f:
+        for line in f:
+            x = json.loads(line)
+            context_keys = sorted([key for key in x.keys() if "context" in key])
+            instance = DatasetInstance(
+                context = [x[key] for key in context_keys], response = x["response"],
+            )
+            instances.append(instance)
+    return instances
+
+
+class RedditData(torch.utils.data.Dataset):
+    def __init__(
+            self,
+            instances: List[DatasetInstance],
+            sp_processor: SentencePieceProcessor,
+            truncation_length: int,
+    ):
+        self.sp_processor = sp_processor
+        self.instances = instances
+        self.truncation_length = truncation_length
+
+    def __len__(self):
+        return len(self.instances)
+
+    def __getitem__(self, item):
+        context_str = self.instances[item].context[0]
+        context_embedding = self._convert_instance_to_embedding(context_str)
+        reply_embedding = self._convert_instance_to_embedding(
+            self.instances[item].response
+        )
+        return EmbeddingPair(context = context_embedding, reply = reply_embedding)
+
+    def _convert_instance_to_embedding(self, input_str: str) -> EncoderInputFeature:
+        input_ids = self.sp_processor.EncodeAsIds(input_str)
+        if self.truncation_length:
+            input_ids = input_ids[: self.truncation_length]
+        attention_mask = [1 for _ in range(len(input_ids))]  # [1]*len(input_ids)
+        position_ids = [i for i in range(len(input_ids))]  # list(range(len(input_ids))
+
+        return EncoderInputFeature(
+            input_ids = torch.tensor(input_ids),
+            attention_mask = torch.tensor(attention_mask),
+            position_ids = torch.tensor(position_ids),
+            input_lengths = torch.tensor(len(input_ids)),
+        )
